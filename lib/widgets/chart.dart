b14ba1a6cef4b9f94a892679d34a9f63481958bf
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2020-09-22 08:54:32 -0300

    developing chart bar

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,10 +1,16 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../model/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
+  static const _AMOUNT = "amount";
+  static const _DAY = "day";
   final List<Transaction> _list;
+
   List<Map<String, Object>> get getGroupTransactions {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(
@@ -18,7 +24,16 @@ class Chart extends StatelessWidget {
           totalAmount += element.amount;
         }
       });
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalAmount};
+      return {
+        _DAY: DateFormat.E().format(weekDay).substring(0, 1),
+        _AMOUNT: totalAmount
+      };
+    });
+  }
+
+  double get maxSpending {
+    return getGroupTransactions.fold(0.0, (sum, element) {
+      return sum + element[_AMOUNT];
     });
   }
 
@@ -30,7 +45,12 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(16.0),
-      child: Row(children: []),
+      child: Row(
+        children: getGroupTransactions.map((e) {
+          return ChartBar(e[_DAY], e[_AMOUNT],
+              maxSpending == 0.0 ? 0.0 : (e[_AMOUNT] as double) / maxSpending);
+        }).toList(),
+      ),
     );
   }
 }
